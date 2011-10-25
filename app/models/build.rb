216046fa5e900bcde73247dc0befc4eb1d00aa08
Marcel de Graaf <mail@marceldegraaf.net>
2011-10-25 22:07:56 +0200

    Add state machine to builds

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,5 +1,28 @@
 class Build < ActiveRecord::Base
 
+  # Relations
+  # ------------------------------------------------------------
+
+  belongs_to :project
+
+  # Validations
+  # ------------------------------------------------------------
+
   validates_presence_of :project_id, :triggered_by_commit
+
+  # States
+  # ------------------------------------------------------------
+
+  state_machine :state, :initial => :pending do
+
+    event :start_build do
+      transition :pending => :building
+    end
+
+    event :finalize_build do
+      transition :building => :built
+    end
+
+  end
 
 end
